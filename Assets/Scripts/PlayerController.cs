d43208fcbdbb93ab1a6c8fa1d8890edaa3c0a5fc
JoaquinGasperetti <143657143+JoaquinGasperetti@users.noreply.github.com>
2024-10-28 14:11:03 -0300

    Camera Update
    Se agrego la cámara en primera persona junto con su movimiento y se quito la función de salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Referencias:")]
+    public Camera playerCamera;
+    [Header("Variables movimiento:")]
     public float WalkSpeed = 5f;
-    public float AlturaSalto = 2f;
-    public float Graity = -20f;
+    public float RunSpeed = 10f;
+    [Header("Variables de rotacion:")]
+    public float SensivilidadCamara;
 
+    //Variables privadas:
     Vector3 MoveInput = Vector3.zero;
     CharacterController charactercontroller;
+    Vector3 InputRotacion = Vector3.zero;
+    private float AnguloVerticalCam;
 
     private void Awake()
     {
@@ -21,22 +28,34 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Move();
+        Mirar();
     }
 
-    private void Move()
+    private void Move() //Funcion de movimiento 
     {
-        if (charactercontroller.isGrounded)
-        {
-            MoveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-            MoveInput = transform.TransformDirection(MoveInput) * WalkSpeed;
+        MoveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-            if (Input.GetButtonDown("Jump"))
-            {
-                MoveInput.y = Mathf.Sqrt(AlturaSalto * 2f * Graity);
-            }
+        if (Input.GetButton("Sprint")) //correr y caminar
+        {
+            MoveInput = transform.TransformDirection(MoveInput) * RunSpeed;
+        } else
+        {
+            MoveInput = transform.TransformDirection(MoveInput) * WalkSpeed;
         }
 
-        MoveInput.y += Graity * Time.deltaTime;
+        
         charactercontroller.Move(MoveInput * Time.deltaTime);
+    }
+
+    private void Mirar() //Funcion de el movimiento de la cámara
+    {
+        InputRotacion.x = Input.GetAxis("Mouse X") * SensivilidadCamara * Time.deltaTime;
+        InputRotacion.y = Input.GetAxis("Mouse Y") * SensivilidadCamara * Time.deltaTime;
+
+        AnguloVerticalCam += InputRotacion.y;
+        AnguloVerticalCam = Mathf.Clamp(AnguloVerticalCam, -70, 70);
+
+        transform.Rotate(Vector3.up * InputRotacion.x);
+        playerCamera.transform.localRotation = Quaternion.Euler(-AnguloVerticalCam, 0f, 0f);
     }
 }
